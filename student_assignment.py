f8c76e1131319aa7b50c0ec038822f8d95d79ccf
Leo Liao <leo_liao@asus.com>
2025-01-24 10:04:02 +0800

    [rag2-hw02] Enhance the RE, try 1

--- student_assignment.py
+++ student_assignment.py
@@ -23,47 +23,37 @@ def hw02_1(q1_pdf):
     return last_chunk
 
 def hw02_2(q2_pdf):
-    result_chunks = []
-
     # Load pdf
     loader = PyPDFLoader(q2_pdf)
     docs = loader.load() # Load as pages
 
     # Merge pages to a single text
-    content = ""
+    full_content = ""
     for doc in docs:
-        content += doc.page_content
+        full_content += doc.page_content
 
-    # Split content into title and remaining
-    title_paragraph_separators = [r"\n.*第\s*一\s*章.*\n"]
-    title_paragraph_splitter = RecursiveCharacterTextSplitter(
-        separators = title_paragraph_separators,
-        keep_separator = "start",
-        chunk_size = 50,
-        chunk_overlap = 0,
-        is_separator_regex = True)
-    title_paragraphs = title_paragraph_splitter.split_text(content)
+    # print(f"full_content: \n{full_content}\n")
 
-    if len(title_paragraphs) != 2: # Title and remaining content
-        print("Fail to parse title")
-        return 0
-
-    title_chunk = title_paragraphs[0]
-    remaining_content = title_paragraphs[1]
-    result_chunks.append(title_chunk)
-
-    # Split remaining into chapter paragraphs
-    chapter_paragraph_separators = [r"(?:\n|^|\s+)第\s*(?:[一二三四五六七八九十百千萬]+|零)\s*章.*\n"]
+    # Split content into chapter paragraphs
+    chapter_paragraph_separators = [r"(?:\n|^|\s*)第\s+(?:[一二三四五六七八九十百千萬]+|零)\s+章.*\n"]
     chapter_paragraph_splitter = RecursiveCharacterTextSplitter(
         separators = chapter_paragraph_separators,
         keep_separator = "start",
         chunk_size = 500,
         chunk_overlap = 0,
         is_separator_regex = True)
-    chapter_paragraphs = chapter_paragraph_splitter.split_text(remaining_content)
+    chapter_paragraphs = chapter_paragraph_splitter.split_text(full_content)
 
-    # Split chapter paragraph into sessions
-    session_separators = [r"(?:\n|^|\s*)第\s*[0-9]+(?:-[0-9]+)?\s*條.*\n"]
+    if len(chapter_paragraphs) <= 0:
+        print("Fail to parse title")
+        return 0
+
+    print(f"chapter_paragraphs (+title): \n{len(chapter_paragraphs)}\n")
+    # print(f"title_chunk: \n{title_chunk}\n")
+
+    # Split chapter paragraph into sessions and put them to total_chunks
+    total_chunks = []
+    session_separators = [r"(?:\n|^|\s*)第\s+[0-9]+(?:-[0-9]+)?\s+條.*\n"]
     session_splitter = RecursiveCharacterTextSplitter(
         separators = session_separators,
         keep_separator = "start",
@@ -72,10 +62,10 @@ def hw02_2(q2_pdf):
         is_separator_regex = True)
     for chapter in chapter_paragraphs:
         session_chunks = session_splitter.split_text(chapter)
-        result_chunks.extend(session_chunks)
+        total_chunks.extend(session_chunks)
 
-    print(f"\nchunks: {len(result_chunks)}")
-    # for index, chunk in enumerate(result_chunks):
-    #     print(f"chunk: {index + 1}:{chunk}\n{'='*20}\n")+
+    print(f"\ntotal_chunks: {len(total_chunks)}")
+    for index, chunk in enumerate(total_chunks):
+        print(f"chunk: {index + 1}:{chunk}\n{'='*20}\n")
 
-    return len(result_chunks)
+    return len(total_chunks)
